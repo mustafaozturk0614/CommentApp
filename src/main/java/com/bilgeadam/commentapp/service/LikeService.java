ce358d1c653055d5301cbd92379d63430758deae
musty1406 <musty1406@gmail.com>
2022-09-27 16:49:51 +0300

    Gün sonu

--- src/main/java/com/bilgeadam/commentapp/service/LikeService.java
+++ src/main/java/com/bilgeadam/commentapp/service/LikeService.java
@@ -1,10 +1,18 @@
 package com.bilgeadam.commentapp.service;
 
+import com.bilgeadam.commentapp.dto.request.LikeCreateRequestDto;
+import com.bilgeadam.commentapp.exception.CommentAppManagerException;
+import com.bilgeadam.commentapp.exception.ErrorType;
+import com.bilgeadam.commentapp.mapper.LikeMapper;
 import com.bilgeadam.commentapp.repository.ILikeRepository;
 import com.bilgeadam.commentapp.repository.entity.Like;
+import com.bilgeadam.commentapp.repository.entity.Product;
+import com.bilgeadam.commentapp.repository.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -12,11 +20,36 @@ import java.util.Optional;
 public class LikeService {
 
 private  final ILikeRepository likeRepository;
+   private final UserService userService;
+
+   private final ProductService productService;
+
+public Like save(LikeCreateRequestDto like){
+   Optional<User> userDb=userService.findById(like.getUserId());
+   Optional<Product> productDb=productService.findById(like.getProductId());
+
+   if (userDb.isEmpty()){
+
+      throw  new CommentAppManagerException(ErrorType.USER_NOT_FOUND,"Kullanýcý Bulunamadý");
+
+   }
+   if (productDb.isEmpty()){
+
+      throw  new CommentAppManagerException(ErrorType.PRODUCT_NOT_FOUND,"Urunu Bulamadik");
+   }
+
+   Like like1= LikeMapper.INSTANCE.toLike(like);
+   likeRepository.save(like1);
+   productDb.get().getLikes().add(like1.getId());
+   productService.save(productDb.get());
+
+   return  like1;
 
 
-public Like save(Like like){
-
-   return likeRepository.save(like);
 }
 
+
+
 }
+
+
